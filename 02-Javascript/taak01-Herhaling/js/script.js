b1c3e86af70dffb5b950862eccc4f599e9bc07b0
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-02-03 14:10:11 +0100

    forntedn essentials 2 2-1

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,24 @@
 let strNaam;
 let boolNaamKlopt;
+var person = prompt("wat is je naam");
+strNaam = person;
+var klopt = confirm("Klopt het wat je hebt gezegt");
+boolNaamKlopt = klopt;
+
+console.log(strNaam);
+console.log(boolNaamKlopt);
+
+if(boolNaamKlopt == true){
+    alert("Welkom")
+}
+else{
+    var person = prompt("wat is je naam");
+    var klopt2 = confirm("Klopt het wat je hebt gezegt");
+};
+boolNaamKlopt2 = klopt2;
+if(boolNaamKlopt2 == false){
+    alert("Er is iets mis gegaan")
+}
+else{
+    alert("Welkom")
+}
