4de1f971967d1981eba2bae3e2f4126e2f4b376b
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-02-09 11:11:33 +0100

    frontend essentials2 2.4

--- 02-Javascript/taak04-Functions-Parameters/js/script.js
+++ 02-Javascript/taak04-Functions-Parameters/js/script.js
@@ -5,4 +5,13 @@ function geefRandomGetalTerug(){                    //randomGetal =
     return randomGetal;
 }
 
+var getal1 = prompt("Voer een getal in");
+var getal2 = prompt("Voer nog een getal in")
+
+function som(){
+return getal1 * getal2;
+
+}
+
 console.log(geefRandomGetalTerug());     
+console.log(som())
