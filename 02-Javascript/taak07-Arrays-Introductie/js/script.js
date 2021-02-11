2205ea7a3aee0b03a961b88c3d51edb2515ba67d
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-02-11 14:20:13 +0100

    frontend essentials2 2-7

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,7 @@
+topGames = ["yakuza_0", "fallout4", "sea_of_thieves"];
+topFilms = ["spongebov_spons_boven_water", "avengers_endgame", "starwars_the_empire_strikes_back"];
+topEten = ["patat", "pizza", "kippenpoten"];
+
+console.log(topGames);
+console.log(topFilms);
+console.log(topEten);
