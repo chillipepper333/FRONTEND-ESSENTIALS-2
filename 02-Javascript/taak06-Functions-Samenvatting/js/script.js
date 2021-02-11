e9b8bc7d89c3f442f34787ecaa64f77a790eb286
Wouter Baas <Wouter.Baas@student.rocva.nl>
2021-02-11 12:41:41 +0100

    frontend essentials 2 2-6

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -1,14 +1,50 @@
 var score = 0;
 let getal1;
+let getal2;
 
 function makeRandomNumber(){
     return Math.floor(Math.random()*99+1);
 }
-
-function showRandomNumber1(){
-    let randomGetal = makeRandomNumber()
-    document.querySelector(".getal1").innerHTML = randomGetal;
-    return randomGetal;
+function setnumbers(){
+    getal1 = makeRandomNumber();
+    getal2 = makeRandomNumber();
 }
 
-getal1 = showRandomNumber1();
+function showRandomNumber(){
+    setnumbers();
+    document.getElementById("getal1").innerHTML = getal1;
+    document.getElementById("getal2").innerHTML = getal2;
+}
+
+showRandomNumber();
+
+function geefAntwoord(status){
+    if(status == "hoger"){
+        if(getal1 > getal2 ){
+            score++;
+            document.querySelector("body").style.backgroundColor = "Green";
+        }
+        else{
+            score--;
+            document.querySelector("body").style.backgroundColor = "Red";
+        }
+    }
+    else{
+        if(getal1 < getal2){
+            score++;
+            document.querySelector("body").style.backgroundColor = "Green";
+        }
+        else{
+            score--;
+            document.querySelector("body").style.backgroundColor = "Red";
+        }
+    }
+    document.getElementById("score").innerHTML = score;
+    showRandomNumber();
+}
+
+
+
+
+
+
